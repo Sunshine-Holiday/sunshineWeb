12ff46d923f519f393523022eccc425267568b27
Rudranayak17 <rudranayak311@gmail.com>
2025-01-27 09:17:22 +0530

    update the code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -79,6 +79,7 @@ navigate("/trips")
         >
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">
             {/* Discover Your Next Adventure */}
+            hello
           </h1>
           <p className="text-xl text-white/90 mb-12">{typedText}</p>
           <CustomButton onclickHandler={getStartedHandler} />
