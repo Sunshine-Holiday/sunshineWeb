e38ba3df60e40aaf8af2f03b00c42563caef5580
Rudranayak17 <rudranayak311@gmail.com>
2025-01-29 19:14:44 +0530

    update the code

--- src/pages/contact/ContactInfo.tsx
+++ src/pages/contact/ContactInfo.tsx
@@ -5,7 +5,7 @@ const contactInfo = [
   {
     icon: MapPin,
     title: "Address",
-    content: "Sr No 53/1 Ashtavinayak Society Sainath Nagar, Vadgaon Sheri, Pune - 411014",
+    content: "Sr No 53/1 Ashtavinayak Chowk, Sainath Nagar, Vadgaon Sheri, Pune - 411014",
     link: "https://g.co/kgs/k6yx7Qr",
   },
   {
