dad0f897add8c0cdc65c52a6e0abfa024cd34b39
Rudranayak17 <rudranayak311@gmail.com>
2025-03-04 22:17:30 +0530

    update the code

--- src/components/footer/SocialLinks.tsx
+++ src/components/footer/SocialLinks.tsx
@@ -5,8 +5,7 @@ import { motion } from 'framer-motion';
 const socialLinks = [
   { icon: Instagram, href: 'https://www.instagram.com/sunshine_holiday_packages', label: 'Instagram' },
   { icon: Facebook, href: 'https://www.facebook.com/profile.php?id=100063907423640&mibextid=ZbWKwL', label: 'Facebook' },
-  { icon: Twitter, href: 'https://x.com', label: 'Twitter' },
-  { icon: Youtube, href: 'https://www.youtube.com/', label: 'Youtube' },
+
 ];
 
 export const SocialLinks = () => {
